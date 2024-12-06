24a83bc5e85e4ca0b562b0047a24adc37ac49d7c
Klaudio <klaudio.pali92@gmail.com>
2024-12-06 17:37:21 -0500

    12/6 webelements/locators added for pages

--- src/test/java/com/orange/pages/PerformancePage.java
+++ src/test/java/com/orange/pages/PerformancePage.java
@@ -1,4 +1,27 @@
 package com.orange.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class PerformancePage {
+
+    WebElement performanceMenu = driver.findElement(By.cssSelector("a[href='/web/index.php/performance/viewPerformanceModule']"));
+performanceMenu.click();
+
+
+    WebElement configureMenu = driver.findElement(By.cssSelector("a[href='/web/index.php/performance/configurePerformance']"));
+configureMenu.click();
+
+
+    WebElement manageReviewsMenu = driver.findElement(By.cssSelector("a[href='/web/index.php/performance/manageReviews']"));
+manageReviewsMenu.click();
+
+
+    WebElement myTrackersMenu = driver.findElement(By.cssSelector("a[href='/web/index.php/performance/myTrackers']"));
+myTrackersMenu.click();
+
+
+    WebElement employeeTrackersMenu = driver.findElement(By.cssSelector("a[href='/web/index.php/performance/employeeTrackers']"));
+employeeTrackersMenu.click();
+
 }
