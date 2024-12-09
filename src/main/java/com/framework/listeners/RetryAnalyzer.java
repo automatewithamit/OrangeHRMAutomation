f35b0c656a7b5199432e71438afd7f7b0bade744
Sheila3557 <gamersthumb721@gmail.com>
2024-12-08 20:18:22 -0800

    committing my feature file and listener package

---
+++ src/main/java/com/framework/listeners/RetryAnalyzer.java
@@ -0,0 +1,30 @@
+package com.framework.listeners;
+
+public class RetryAnalyzer {
+
+
+
+import org.testng.ITestResult;
+import org.testng.IRetryAnalyzer;
+
+    public class RetryAnalyzer implements IRetryAnalyzer {
+
+        int counter = 0;
+        // int retryLimit = 3;
+
+        public boolean retry(ITestResult result) {
+            PropertiesHelper configProperty = new PropertiesHelper("//resources//config.properties");
+            String retryLimit = configProperty.getProperty("maxRetryCount");
+            // String retryLimit =
+            // Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("retry");
+            System.out.println("Retry Count --> " + retryLimit);
+
+            if (counter < Integer.valueOf(retryLimit)) {
+                counter++;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
