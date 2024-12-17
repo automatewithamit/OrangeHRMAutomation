c71e3f95ba17e9f5f0e1938d60dc2babf7ecfdb4
Sheila3557 <gamersthumb721@gmail.com>
2024-12-16 23:17:25 -0800

    commit dashboard feature and steps

--- src/test/java/Steps/DashboardSteps.java
+++ src/test/java/Steps/DashboardSteps.java
@@ -1,4 +1,147 @@
 package Steps;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class DashboardSteps {
+
+
+    @Given("the user logs in with {string} and {string}")
+    public void the_user_logs_in_with_and(String string, String string2) {
+    }
+
+    @Given("the user is redirected to the dashboard page")
+    public void the_user_is_redirected_to_the_dashboard_page() {
+    }
+    @Then("the dashboard should display the header with the application logo")
+    public void the_dashboard_should_display_the_header_with_the_application_logo() {
+    }
+    @Then("the dashboard should display the user's profile information")
+    public void the_dashboard_should_display_the_user_s_profile_information() {
+    }
+    @Then("all widgets on the dashboard should be visible")
+    public void all_widgets_on_the_dashboard_should_be_visible() {
+    }
+    @Then("the navigation menu should be functional")
+    public void the_navigation_menu_should_be_functional() {
+    }
+    @Then("no error messages should be displayed")
+    public void no_error_messages_should_be_displayed() {
+    }
+    @Given("the \"Time at Work\" widget is loaded on the dashboard")
+    public void the_time_at_work_widget_is_loaded_on_the_dashboard() {
+    }
+    @When("the user views the \"Time at Work\" widget")
+    public void the_user_views_the_time_at_work_widget() {
+    }
+    @Then("the widget should display the total working hours for the day")
+    public void the_widget_should_display_the_total_working_hours_for_the_day() {
+    }
+    @And("the displayed hours should match the recorded work hours in the system")
+    public void the_displayed_hours_should_match_the_recorded_work_hours_in_the_system() {
+    }
+    @And("the widget should update dynamically if the user logs additional time")
+    public void the_widget_should_update_dynamically_if_the_user_logs_additional_time() {
+    }
+    @Given("the \"My Actions\" section is visible on the dashboard")
+    public void the_my_actions_section_is_visible_on_the_dashboard() {
+    }
+    @When("the user views the \"My Actions\" section")
+    public void the_user_views_the_my_actions_section() {
+    }
+    @Then("the section should display a list of pending tasks")
+    public void the_section_should_display_a_list_of_pending_tasks() {
+    }
+    @And("the total number of tasks displayed should match the records in the system")
+    public void the_total_number_of_tasks_displayed_should_match_the_records_in_the_system() {
+    }
+    @Given("the user is viewing the dashboard on a mobile device")
+    public void the_user_is_viewing_the_dashboard_on_a_mobile_device() {
+    }
+    @Then("all widgets should resize to fit the mobile screen dimensions")
+    public void all_widget_should_resize_to_fit_the_mobile_screen_dimensions() {
+    }
+    @And("the navigation menu should be visible and functional")
+    public void the_navigation_menu_should_be_visible_and_functional() {
+    }
+    @And("all elements should remain interactive")
+    public void all_elements_should_remain_interactive() {
+    }
+    @Given("a location has no employees assigned")
+    public void a_location_has_no_employees_assigned() {
+    }
+    @When("the user views the pie chart")
+    public void the_user_views_the_pie_chart() {
+    }
+    @Then("the chart should display a segment for the location with \"Unassigned\"")
+    public void the_chart_should_display_a_segment_for_the_location_with_unassigned() {
+    }
+    @And("the chart should handle the scenario without visual errors")
+    public void the_chart_should_handle_the_scenario_without_visual_errors() {
+    }
+    @Given("the \"Help\" button is displayed on the dashboard")
+    public void the_help_button_is_displayed_on_the_dashboard() {
+    }
+    @When("the user views the dashboard")
+    public void the_user_views_the_dashboard() {
+    }
+    @Then("the \"Help\" button should be visible")
+    public void the_help_button_should_be_visible() {
+    }
+    @And("the button should be focusable using the keyboard")
+    public void the_button_should_be_focusable_using_the_keyboard() {
+    }
+    @Given("the user remains inactive on the dashboard for the session timeout duration")
+    public void the_user_remains_inactive_on_the_dashboard_for_the_session_timeout_duration() {
+    }
+    @When("the session expires")
+    public void the_session_expires() {
+    }
+    @Then("the user should be redirected to the login page")
+    public void the_user_should_be_redirected_to_the_login_page() {
+    }
+    @And("the login page should display a message \"Your session has expired. Please log in again.\"")
+    public void the_login_page_should_display_a_message_your_session_has_expired_please_log_in_again() {
+    }
+    @Given("the user is viewing the dashboard on a tablet device")
+    public void the_user_is_viewing_the_dashboard_on_a_tablet_device() {
+    }
+    @Then("all widgets should resize to fit the tablet screen dimensions")
+    public void all_widgets_should_resize_to_fit_the_tablet_screen_dimensions() {
+    }
+    @And("all interactive elements should remain functional")
+    public void all_interactive_elements_should_remain_functional() {
+    }
+    @Given("the notification panel is in an expanded state")
+    public void the_notification_panel_is_in_an_expanded_state() {
+    }
+    @When("the user clicks the panel toggle button")
+    public void the_user_clicks_the_panel_toggle_button() {
+    }
+    @Then("the notification panel should collapse smoothly")
+    public void the_notification_panel_should_collapse_smoothly() {
+    }
+    @And("the toggle button should remain visible")
+    public void the_toggle_button_should_remain_visible() {
+    }
+    @Given("there are no employees on leave for today")
+    public void there_are_no_employees_on_leave_for_today() {
+    }
+    @When("the widget loads")
+    public void the_widget_loads() {
+    }
+    @Then("the widget should display a message \"No employees on leave today\"")
+    public void the_widget_should_display_a_message_no_employees_on_leave_today() {
+    }
+    @And("no employee entries should be listed")
+    public void no_employee_entries_should_be_listed() {
+    }
 }
+
+
+
+
+
+
