96f2e7c5ce064970ca376bfdda42c8f49f1112e3
AlexMAAK <alexmaak17121991@gmail.com>
2025-01-04 22:33:00 -0500

    updated Admin feature file and Admin Steps

--- src/test/java/Steps/AdminSteps.java
+++ src/test/java/Steps/AdminSteps.java
@@ -1,10 +1,35 @@
 package Steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class AdminSteps {
 
-    public void mergeconflictfrommain(){
-
+    @Given("I am logged in as an admin")
+    public void i_am_logged_in_as_an_admin() {
     }
+
+    @When("I navigate to the {string} module")
+    public void i_navigate_to_the_module(String string) {
+    }
+
+    @When("I fill in all required fields with valid data")
+    public void i_fill_in_all_required_fields_with_valid_data() {
+    }
+
+    @When("I click {string}")
+    public void i_click(String string) {
+    }
+
+    @Then("the new user should be successfully added")
+    public void the_new_user_should_be_successfully_added() {
+    }
+
+    @Then("a confirmation message should be displayed")
+    public void a_confirmation_message_should_be_displayed() {
+    }
+
 
 
 }
